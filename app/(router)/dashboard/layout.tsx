fd7aadbc143a56ba8cfce74463f973926d57eb99
SUBBARAYUDU3524 <subburajupalem2222@gmail.com>
2025-08-16 20:59:02 +0530

    meta data changed

--- app/(router)/dashboard/layout.tsx
+++ app/(router)/dashboard/layout.tsx
@@ -3,8 +3,8 @@ import Navbar from './_components/Navbar';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'My App',
-  description: 'A Next.js app with a constant navbar',
+  title: 'QUOTENXT',
+  description: 'Quotenxt IT Solutions delivers expert IT services, cloud solutions, software development, cybersecurity, and digital transformation strategies to help businesses grow and stay ahead in the digital world.',
 };
 
 export default function RootLayout({
